5163d96226d80ddb2b871a79fe0ab82d935e6c58
Danny Nguyen <danny@80pct.com>
2016-07-11 15:38:33 -0400

    Implemented raindrops exercise

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,17 @@
+module BookKeeping
+    VERSION = 2
+end
+
+class Raindrops
+  def self.convert(n)
+    words = []
+    words << "Pling" if n % 3 == 0
+    words << "Plang" if n % 5 == 0
+    words << "Plong" if n % 7 == 0
+    if words.length == 0
+      n.to_s
+    else
+      words.join
+    end
+  end
+end
