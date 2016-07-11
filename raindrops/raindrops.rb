1dfaf062f7e73bcaa3f102f71f5d9915e29dfc3c
Danny Nguyen <danny@80pct.com>
2016-07-11 15:47:27 -0400

    Use string to store temp value instead

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -4,14 +4,14 @@ end
 
 class Raindrops
   def self.convert(n)
-    words = []
+    words = ""
     words << "Pling" if n % 3 == 0
     words << "Plang" if n % 5 == 0
     words << "Plong" if n % 7 == 0
     if words.length == 0
       n.to_s
     else
-      words.join
+      words
     end
   end
 end
